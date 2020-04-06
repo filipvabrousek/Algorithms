15e56f40c971ae0c55269bed622a0cbef1b7a475
Filip Vabrousek <filipvabrousek1@gmail.com>
2020-04-06 20:43:37 +0200

    Update Reactive.js

--- My/Reactive.js
+++ My/Reactive.js
@@ -1,14 +1,14 @@
 var person = {
-		name: "Filip"
-	}
+	name: "Filip"
+}
 
-	React(person, "name", "h1");
+React(person, "name", "h1");
 
-	function React(obj, key, el) {
-		document.querySelector(el).innerText = obj[key];
-		Object.defineProperty(obj, key, {
-			set(newVal) {
-				document.querySelector(el).innerText = newVal;
-			}
-		});
+function React(obj, key, el){
+	document.querySelector(el).innerText = obj[key];
+	Object.defineProperty(obj, key, {
+		set(newVal){
+		document.querySelector(el).innerText = newVal;
 	}
+	});
+}
